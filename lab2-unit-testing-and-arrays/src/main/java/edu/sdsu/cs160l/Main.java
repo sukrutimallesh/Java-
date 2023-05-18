4e5702fb7824b02455ad41f4a85f7519ce3dab82
Sukruti Mallesh <sukrutimallesh@gmail.com>
2023-05-18 09:30:24 +0200

    Add files via upload

---
+++ lab2-unit-testing-and-arrays/src/main/java/edu/sdsu/cs160l/Main.java
@@ -0,0 +1,14 @@
+package edu.sdsu.cs160l;
+
+/**
+ *  This lab will introduce you to unit testing and refresh your memory about arrays.
+ *  We will spend the
+ */
+public class Main {
+    public static void main(String[] args) {
+        //To create a student
+        Student hmac = new Student(825027067L, "Harsh Maheshwari", 3.7);
+        // printing an object directly would invoke the objects toString method
+        System.out.println(hmac);
+    }
+}
